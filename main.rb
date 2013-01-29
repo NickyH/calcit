4a08f2e2cffb8d59daf5bcd7adcc0cb025feb193
NickyH <nickyhughes00@gmail.com>
2013-01-29 15:43:29 -0500

    Added basic calc function

--- main.rb
+++ main.rb
@@ -13,6 +13,21 @@ while quit == "no"
     when "1"
       #Enter basic calculator script here
 
+      puts "What operation would you like to do? Add(a), Subtract(s), Muliply(m) or Divide(d)?"
+      op = gets.chomp
+
+      puts "What is the first number?"
+      num1 = gets.chomp.to_i
+
+      puts "What is the second number?"
+      num2 = gets.chomp.to_i
+
+      case op
+        when 'm' #call multiply def
+        when 'a' #call add def
+        when 's' #call subtract def
+        when 'd' #call divide def
+      end
 
 
     when "2"
