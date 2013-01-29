617d50811312e183b32a293daf2805f12c86bca5
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-01-29 15:52:03 -0500

    moved the definitions up top

--- main.rb
+++ main.rb
@@ -1,3 +1,42 @@
+#Definitions
+  #Basic
+    def add(first, second)
+      first + second
+    end
+
+    def subtract(first, second)
+      first - second
+    end
+
+    def multiply(first, second)
+      first*second
+    end
+
+    def divide(first, second)
+      first/second
+    end
+  #Advanced
+    def power(first, second)
+      first**second
+    end
+
+    def squareroot(first)
+      Math.sqrt(first)
+    end
+
+    # The outputs of the functions are the following
+    # puts add(1, 2) => 3
+
+    # puts subtract(1, 3) => -2
+
+    # puts multiply(2, 2) => 4
+
+    # puts divide(6, 2) => 3
+
+    # puts power(2, 3) => 8
+
+    # puts squareroot(4) => 2
+
 #Script
   #Introduction
 quit == "no"
@@ -44,42 +83,6 @@ while quit == "no"
   quit = gets.chomp
 end
 
-#Definitions
-  #Basic
-    def add(first, second)
-      first + second
-    end
 
-    def subtract(first, second)
-      first - second
-    end
 
-    def multiply(first, second)
-      first*second
-    end
 
-    def divide(first, second)
-      first/second
-    end
-  #Advanced
-    def power(first, second)
-      first**second
-    end
-
-    def squareroot(first)
-      Math.sqrt(first)
-    end
-
-    # To see how the code works, uncomment one of the following
-    #and then press command b
-    # puts add(1, 2) => 3
-
-    # puts subtract(1, 3) => -2
-
-    # puts multiply(2, 2) => 4
-
-    # puts divide(6, 2) => 3
-
-    # puts power(2, 3) => 8
-
-    # puts squareroot(4) => 2
