dac0d47d5652c86b93771936608e0f3490c0d5a1
Ryan Lee <rlee1121@gmail.com>
2013-01-29 15:35:16 -0500

    added intro script

--- main.rb
+++ main.rb
@@ -1,9 +1,33 @@
 #Script
   #Introduction
+quit == "no"
 
-  # Basic Calculator Script
+while quit == "no"
 
-  #Advanced Calculator Script
+  print "Type '1' for the basic calculator, type '2' for the advanced calculator. "
+  calc_type = gets.chomp
+
+
+
+  case calc_type
+    when "1"
+      #Enter basic calculator script here
+
+
+
+    when "2"
+      #Enter advanced calculator script here
+
+
+
+    else
+      print "Please enter a '1' for basic calculator or a '2' for the advanced calculator. "
+
+  end
+
+  print "Would you like to quit (yes/no)? "
+  quit = gets.chomp
+end
 
 #Definitions
   #Basic
