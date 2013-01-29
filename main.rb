bb5a2b8e3d5722221c8642c37d2b17cc6691583d
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-01-29 15:38:38 -0500

    added the functions

--- main.rb
+++ main.rb
@@ -7,5 +7,40 @@
 
 #Definitions
   #Basic
+    def add(first, second)
+      first + second
+    end
 
+    def subtract(first, second)
+      first - second
+    end
+
+    def multiply(first, second)
+      first*second
+    end
+
+    def divide(first, second)
+      first/second
+    end
   #Advanced
+    def power(first, second)
+      first**second
+    end
+
+    def squareroot(first)
+      Math.sqrt(first)
+    end
+
+    # To see how the code works, uncomment one of the following
+    #and then press command b
+    # puts add(1, 2)
+
+    # puts subtract(1, 3)
+
+    # puts multiply(2, 2)
+
+    # puts divide(6, 2)
+
+    # puts power(2, 3)
+
+    # puts squareroot(4)
